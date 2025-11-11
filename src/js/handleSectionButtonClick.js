d86405b26eb3e6df4d23671b27b8ef6ebc0367e5
Damien <damienroot@icloud.com>
2025-11-11 09:51:30 +0000

    create input form

--- src/js/handleSectionButtonClick.js
+++ src/js/handleSectionButtonClick.js
@@ -4,17 +4,23 @@ const sectionButtonSummary = document.querySelector(".sectionButtonSummary");
 const sectionIncoming = document.querySelector(".sectionIncoming");
 const sectionOutgoing = document.querySelector(".sectionOutgoing");
 const sectionSummary = document.querySelector(".sectionSummary");
+const sentToFrom = document.querySelector(".sentToFrom");
+const timeReceivedSent = document.querySelector(".timeReceivedSent");
 
 sectionButtonIncoming.addEventListener("click", () => {
   sectionIncoming.classList.remove("hidden");
   sectionOutgoing.classList.add("hidden");
   sectionSummary.classList.add("hidden");
+  sentToFrom.textContent = "Sent from";
+  timeReceivedSent.textContent = "Time received";
 });
 
 sectionButtonOutgoing.addEventListener("click", () => {
   sectionOutgoing.classList.remove("hidden");
   sectionSummary.classList.add("hidden");
   sectionIncoming.classList.add("hidden");
+  sentToFrom.textContent = "Sending to";
+  timeReceivedSent.textContent = "Time sent";
 });
 
 sectionButtonSummary.addEventListener("click", () => {
